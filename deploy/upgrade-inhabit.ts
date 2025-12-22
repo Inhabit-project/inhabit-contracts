89377541a1685dd856ffc64ce10a66c9e8694577
salviega <salviega6@gmail.com>
2025-12-22 14:47:07 -0500

    ⚡️ Improved upgrade contract script

--- deploy/upgrade-inhabit.ts
+++ deploy/upgrade-inhabit.ts
@@ -6,10 +6,12 @@ import {
 	Deployment,
 	ExtendedArtifact
 } from 'hardhat-deploy/dist/types'
+import { Address } from 'viem'
 
 import { developmentChains } from '@/config/const'
 import { getImplementationAddress } from '@/utils/upgrades/get-implementation-address'
 import { verify } from '@/utils/verify'
+import { wait } from '@/utils/wait'
 
 const upgradeInhabit: DeployFunction = async function (
 	hre: HardhatRuntimeEnvironment
@@ -23,10 +25,10 @@ const upgradeInhabit: DeployFunction = async function (
 	log('-----------------------------------')
 	log('Upgrading Inhabit...')
 
-	const Inhabit: ContractFactory = await ethers.getContractFactory('Inhabit')
+	const inhabit: ContractFactory = await ethers.getContractFactory('Inhabit')
 
 	try {
-		await upgrades.forceImport(proxyAddress, Inhabit, {
+		await upgrades.forceImport(proxyAddress, inhabit, {
 			kind: 'transparent'
 		})
 		log('Proxy imported successfully')
@@ -34,27 +36,39 @@ const upgradeInhabit: DeployFunction = async function (
 		log('Proxy already registered or import not needed')
 	}
 
-	const upgradedProxy = await upgrades.upgradeProxy(proxyAddress, Inhabit)
+	log('-----------------------------------')
+	log('Upgrading Inhabit...')
 
-	await upgradedProxy.getAddress()
+	const upgradedProxy = await upgrades.upgradeProxy(proxyAddress, inhabit, {
+		redeployImplementation: 'always'
+	})
 
-	const implementationAddress: string =
-		await getImplementationAddress(proxyAddress)
+	const upgradedProxyAddress = await upgradedProxy.getAddress()
+	await wait(5)
 
-	log(`New Inhabit implementation deployed at: ${implementationAddress}`)
+	const newImplementationAddress = await getImplementationAddress(
+		upgradedProxyAddress as Address
+	)
+
+	log(`New Inhabit implementation deployed at: ${newImplementationAddress}`)
 
 	if (!developmentChains.includes(network.name)) {
-		await verify(implementationAddress, [])
+		await verify(newImplementationAddress, [])
 	}
 
 	const artifact: ExtendedArtifact =
 		await deployments.getExtendedArtifact('Inhabit')
-	await save('Inhabit', { address: proxyAddress, ...artifact })
+
+	await save('Inhabit', {
+		address: proxyAddress,
+		implementation: newImplementationAddress,
+		...artifact
+	})
 
 	log('-----------------------------------')
 	log(`Inhabit upgraded successfully!`)
 	log(`Proxy address: ${proxyAddress}`)
-	log(`New implementation: ${implementationAddress}`)
+	log(`New implementation: ${newImplementationAddress}`)
 }
 
 upgradeInhabit.tags = ['upgrade']
